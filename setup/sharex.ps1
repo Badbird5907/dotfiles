312ec1235f4d53b2c4518408f977350ae97990e9
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-01-02 22:22:12 -0500

    fix: onedrive bug

--- setup/sharex.ps1
+++ setup/sharex.ps1
@@ -10,15 +10,15 @@ if (-not (Test-Path $folderPath)) {
         Write-Host "Could not find ShareX config folder!" -ForegroundColor Red
         return
     }
+}
 
-    # symlink hotkeys config
-    try {
-        if (-not $sharexHotkeysPath) {
-            throw "ShareX hotkeys path parameter was not specified"
-        }
-        New-Item -ItemType SymbolicLink -Path "$folderPath/HotkeysConfig.json" -Target $sharexHotkeysPath -Force
-        Write-Host "Symlinked ShareX hotkeys config file to $folderPath/HotkeysConfig.json" -ForegroundColor Green
-    } catch {
-        Write-Host "Failed to create symlink for ShareX hotkeys config: $_" -ForegroundColor Red
+# symlink hotkeys config
+try {
+    if (-not $sharexHotkeysPath) {
+        throw "ShareX hotkeys path parameter was not specified"
     }
+    New-Item -ItemType SymbolicLink -Path "$folderPath/HotkeysConfig.json" -Target $sharexHotkeysPath -Force
+    Write-Host "Symlinked ShareX hotkeys config file to $folderPath/HotkeysConfig.json" -ForegroundColor Green
+} catch {
+    Write-Host "Failed to create symlink for ShareX hotkeys config: $_" -ForegroundColor Red
 }
